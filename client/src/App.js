dcd54991deefda194dc788bb46a9b0815cd71df2
jpenagos32 <jpenagos32@gmail.com>
2023-05-26 16:48:22 -0500

    Agregando landing page y actualizando dependencias

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+// import { useState } from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
-  );
+	/* 
+  ! Esta es la funcion que cambia entre dark y light
+	const [darkMode, setDarkMode] = useState(false);
+
+	const darkModeHandler = () => {
+		setDarkMode(!darkMode);
+	};
+ */
+	return (
+		<div>
+			{/*<button onClick={darkModeHandler}>Toggle Dark Mode</button> */}
+			<LandingPage />
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+// import styles from './LandingPage.module.css';
+const LandingPage = (props) => {
+	return (
+		<div>
+			<h1>WELCOME</h1>
+			<h1>Hdenry Foods</h1>
+			<h1>hola</h1>
+		</div>
+	);
+};
+export default LandingPage;
